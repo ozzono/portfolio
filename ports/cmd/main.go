c619ed1b483a84799953f5a43b5c80771b2ab9c1
Hugo Virgilio <huvirgilio@gmail.com>
2022-03-04 21:54:27 -0300

    adding description and main

---
+++ ports/cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"ports/internal/server"
+	"ports/log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	Version            = "1.0.0"
+	PostgresqlUser     = "postgres"
+	PostgresqlPassword = "postgres"
+	PostgresqlHost     = "localhost"
+	PostgresqlPort     = "5432"
+	UserPostgresqlDB   = "ports_service"
+	PostgresqlSSLMode  = "disable"
+)
+
+func main() {
+	logger := log.New().With(context.TODO(), "version", Version)
+	dsn := "host=localhost user=postgres password=postgres dbname=ports_service port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("gorm.Open err", err)
+	}
+	defer func() {
+		sqlDB, err := db.DB()
+		if err != nil {
+			logger.Error(err)
+			return
+		}
+		if err := sqlDB.Close(); err != nil {
+			logger.Error(err)
+		}
+	}()
+
+	s := server.NewServer(logger, db)
+	logger.Info("starting server")
+	s.Run()
+}
